a64d0d501dc9d1a85d1a91787704149c6516a8a7
jaehoo <im.jaehoo1@gmail.com>
2025-02-10 19:54:45 +0900

    refactor: @ alias 설정

--- apps/frontend/src/routes/customers/index.tsx
+++ apps/frontend/src/routes/customers/index.tsx
@@ -1,5 +1,5 @@
+import { CustomersPage } from "@/pages/customers/CustomersPage";
 import { createFileRoute } from "@tanstack/react-router";
-import { CustomersPage } from "../../pages/customers/CustomersPage";
 
 export const Route = createFileRoute("/customers/")({
   component: RouteComponent,
--- apps/frontend/src/routes/purchase-frequency/index.tsx
+++ apps/frontend/src/routes/purchase-frequency/index.tsx
@@ -1,5 +1,5 @@
+import { PurchaseFrequencyPage } from "@/pages/purchase-frequency/PurchaseFrequencyPage";
 import { createFileRoute } from "@tanstack/react-router";
-import { PurchaseFrequencyPage } from "../../pages/purchase-frequency/PurchaseFrequencyPage";
 
 export const Route = createFileRoute("/purchase-frequency/")({
   component: RouteComponent,
